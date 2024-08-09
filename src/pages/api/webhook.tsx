049e0e64321a6bdccd0c759fb149843b716d4f02
nathanliow <nathanliow7456@gmail.com>
2024-08-09 18:31:00 -0500

    fix write after end

--- src/pages/api/webhook.tsx
+++ src/pages/api/webhook.tsx
@@ -11,47 +11,47 @@ export const config = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        const rawBody = await buffer(req);
-        const sig = req.headers['x-payload-digest'];
-        const secretKey = process.env.SUMSUB_WEBHOOK_SECRET_KEY || '';
-        const calculatedDigest = crypto.createHmac("sha256", secretKey)
-            .update(rawBody)
-            .digest('hex');
+        try {
+            const rawBody = await buffer(req);
+            const sig = req.headers['x-payload-digest'];
+            const secretKey = process.env.SUMSUB_WEBHOOK_SECRET_KEY || '';
+            const calculatedDigest = crypto.createHmac("sha256", secretKey)
+                .update(rawBody)
+                .digest('hex');
 
-    const body = JSON.parse(rawBody.toString());
-    const { externalUserId, reviewResult, correlationId } = body;
+            const body = JSON.parse(rawBody.toString());
+            const { externalUserId, reviewResult, correlationId } = body;
 
-    // if (calculatedDigest !== sig) {
-    //     return res.status(400).send(`Invalid signature: ${correlationId}`);
-    // }
+            // if (calculatedDigest !== sig) {
+            //     return res.status(400).send(`Invalid signature: ${correlationId}`);
+            // }
 
-    if (!externalUserId || !reviewResult) {
-        return res.status(400).json({ error: 'Missing required fields' });
-    }
+            if (!externalUserId || !reviewResult) {
+                return res.status(400).json({ error: 'Missing required fields' });
+            }
 
-    const reviewAnswer = reviewResult.reviewAnswer;
+            const reviewAnswer = reviewResult.reviewAnswer;
 
-    let newKycStatus = "pending";
-    if (reviewAnswer === 'GREEN') {
-        newKycStatus = 'approved';
-    } else {
-        if (reviewResult.reviewRejectType == "RETRY") {
-            newKycStatus = 'tempReject';
-        } else if (reviewResult.reviewRejectType == "FINAL") {
-            newKycStatus = 'finalReject';
+            let newKycStatus = "pending";
+            if (reviewAnswer === 'GREEN') {
+                newKycStatus = 'approved';
+            } else {
+                if (reviewResult.reviewRejectType == "RETRY") {
+                    newKycStatus = 'tempReject';
+                } else if (reviewResult.reviewRejectType == "FINAL") {
+                    newKycStatus = 'finalReject';
+                }
+            }
+            
+            // update user kycStatus
+            await updateKycStatus(externalUserId, newKycStatus, reviewResult);
+            res.status(200).json({ message: 'KYC status updated successfully' });
+        } catch (error) {
+            console.error('Error updating KYC status:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
         }
+    } else {
+        res.setHeader('Allow', 'POST');
+        res.status(405).end(`Method Not Allowed`);
     }
-    
-    // update user kycStatus
-    try {
-        await updateKycStatus(externalUserId, newKycStatus, reviewResult);
-        res.status(200).json({ message: 'KYC status updated successfully' });
-    } catch (error) {
-        console.error('Error updating KYC status:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end(`Method Not Allowed`);
-  }
 }
