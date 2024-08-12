763f1ef4a52edd2ad4d22c116ed42f17999a4a20
nathanliow <nathanliow7456@gmail.com>
2024-08-12 13:01:45 -0500

    test webhook

--- src/pages/api/webhook.tsx
+++ src/pages/api/webhook.tsx
@@ -1,4 +1,3 @@
-import { buffer } from 'micro'
 import { NextApiRequest, NextApiResponse } from 'next';
 import crypto from 'crypto';
 import { updateKycStatus } from '@/firebase';
@@ -12,46 +11,60 @@ export const config = {
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         try {
-            const rawBody = await buffer(req);
-            const sig = req.headers['x-payload-digest'];
-            const secretKey = process.env.SUMSUB_WEBHOOK_SECRET_KEY || '';
-            const calculatedDigest = crypto.createHmac("sha256", secretKey)
-                .update(rawBody)
-                .digest('hex');
+            const chunks: Buffer[] = [];
+            
+            req.on('data', chunk => {
+                chunks.push(chunk);
+            });
 
-            const body = JSON.parse(rawBody.toString());
-            const { externalUserId, reviewResult, correlationId } = body;
+            req.on('end', async () => {
+                const rawBody = Buffer.concat(chunks);
 
-            // if (calculatedDigest !== sig) {
-            //     return res.status(400).send(`Invalid signature: ${correlationId}`);
-            // }
+                const sig = req.headers['x-payload-digest'] as string;
+                const secretKey = process.env.SUMSUB_WEBHOOK_SECRET_KEY || '';
 
-            if (!externalUserId || !reviewResult) {
-                return res.status(400).json({ error: 'Missing required fields' });
-            }
+                const calculatedDigest = crypto.createHmac('sha256', secretKey)
+                    .update(rawBody) 
+                    .digest('hex');
 
-            const reviewAnswer = reviewResult.reviewAnswer;
+                console.log('Calculated Digest:', calculatedDigest);
+                console.log('Received Signature:', sig);
 
-            let newKycStatus = "pending";
-            if (reviewAnswer === 'GREEN') {
-                newKycStatus = 'approved';
-            } else {
-                if (reviewResult.reviewRejectType == "RETRY") {
+                const body = JSON.parse(rawBody.toString());
+
+                const { externalUserId, reviewResult } = body;
+
+                if (calculatedDigest !== sig) {
+                    return res.status(400).send(`Invalid signature`);
+                }
+
+                // Check for required fields
+                if (!externalUserId || !reviewResult) {
+                    return res.status(400).json({ error: 'Missing required fields' });
+                }
+
+                const reviewAnswer = reviewResult.reviewAnswer;
+
+                // Determine the new KYC status
+                let newKycStatus = 'pending';
+                if (reviewAnswer === 'GREEN') {
+                    newKycStatus = 'approved';
+                } else if (reviewResult.reviewRejectType === 'RETRY') {
                     newKycStatus = 'tempReject';
-                } else if (reviewResult.reviewRejectType == "FINAL") {
+                } else if (reviewResult.reviewRejectType === 'FINAL') {
                     newKycStatus = 'finalReject';
                 }
-            }
-            
-            // update user kycStatus
-            await updateKycStatus(externalUserId, newKycStatus, reviewResult);
-            res.status(200).json({ message: 'KYC status updated successfully' });
+
+                // Update the user's KYC status in your database
+                await updateKycStatus(externalUserId, newKycStatus, reviewResult);
+                res.status(200).json({ message: 'KYC status updated successfully' });
+            });
         } catch (error) {
             console.error('Error updating KYC status:', error);
             res.status(500).json({ error: 'Internal Server Error' });
         }
     } else {
         res.setHeader('Allow', 'POST');
-        res.status(405).end(`Method Not Allowed`);
+        res.status(405).end('Method Not Allowed');
     }
 }
